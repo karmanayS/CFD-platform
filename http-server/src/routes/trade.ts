1ed73a507579a49534df87becab76654266b42ae
Karmanay Sharma <karmanaysharmaog@gmail.com>
2025-12-23 23:59:24 +0530

    create order almost

--- http-server/src/routes/trade.ts
+++ http-server/src/routes/trade.ts
@@ -9,53 +9,69 @@ tradeRouter.post("/create",authMiddlware,async(req,res) => {
     const {asset,type,qty,leverage} = req.body;
     const randomId = crypto.randomUUID();
 
-    while (true) {     
-        try {
-            const response = await redis.xRevRange('EN-EX', '+', '-', {COUNT: 1});
-            let lastId;
-            if (response.length === 0) {
-                lastId = "0"
-            } else {
-                lastId = response[0].id
-            }
-            
-            await redis.xAdd('EX-EN', '*', {
-                randomId,
-                type: "openOrder",
-                payload :JSON.stringify({
-                    asset,
-                    type,
-                    qty,
-                    leverage,
-                    userId
-                })
-            })
-
-            
-            const data = await redis.xRead({
-                key: "EN-EX",
-                id : lastId
-            }, {
-                BLOCK : 0
-            })
-            
-            if (data) {
-                //@ts-ignore
-                const message = data[0].messages[0].message;
-                if (message.randomId !== randomId) continue;
-                const response = JSON.parse(message.payload).orderId;
-                //@ts-ignore
-                if (message.type === "orderId") {
-                    if (response === "ERROR") throw new Error("Invalid balance")
-                    else {return res.json({orderId : response});}
-                }
-            } else throw new Error("didnt receive orderId from the redis stream");   
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({message : "error while creating oder"})
+    try {
+        const response = await redis.xRevRange('EN-EX', '+', '-', {COUNT: 1});
+        let lastId;
+        if (response.length === 0) {
+            lastId = "0"
+        } else {
+            lastId = response[0].id
         }
+        
+        await redis.xAdd('EX-EN', '*', {
+            randomId,
+            type: "openOrder",
+            payload :JSON.stringify({
+                asset,
+                type,
+                qty,
+                leverage,
+                userId
+            })
+        })
+
+        
+        const allMessages = await redis.xRead({
+            key: "EN-EX",
+            id : lastId
+        }, {
+            BLOCK : 0
+        })
+        if (!allMessages) return res.json({
+            success: false,
+            message: "Did not receive newly created order from engine"
+        })
+        //@ts-ignore
+        const message = allMessages[0].messages.find((entry) => {
+            console.log(entry)
+            if (entry.message.randomId === randomId) {      
+                return entry.message
+            }
+        })
+        
+        if (!message) return res.json({
+            success: false,
+            message : "Couldn't fetch newly created order"
+        })
+        return res.json({
+            success : true,
+            orderId: JSON.parse(message.message.payload).orderId
+        })
+            // if (data) {
+            //     //@ts-ignore
+            //     const message = data[0].messages[0].message;
+            //     if (message.randomId !== randomId) continue;
+            //     const response = JSON.parse(message.payload).orderId;
+            //     //@ts-ignore
+            //     if (message.type === "orderId") {
+            //         if (response === "ERROR") throw new Error("Invalid balance")
+            //         else {return res.json({orderId : response});}
+            //     }
+            // } else throw new Error("didnt receive orderId from the redis stream");   
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({success: false,message : "error while creating oder"})
     }       
-    
 })
 
 tradeRouter.post("/close",authMiddlware,async(req,res) => {
