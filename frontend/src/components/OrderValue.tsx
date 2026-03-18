867506f596357cd4bd70653944973fbda7fbf011
Karmanay Sharma <karmanaysharmaog@gmail.com>
2026-03-18 10:21:34 +0530

    fix: OrderValue BUY SELL

--- frontend/src/components/OrderValue.tsx
+++ frontend/src/components/OrderValue.tsx
@@ -1,13 +1,8 @@
 import type { AssetPrice } from "../hooks/usePriceFeed"
 
 export const OrderValue = ({qty,leverage,asset,orderType,assetPrices}:{qty:number,leverage:number,asset:string,orderType:string,assetPrices:AssetPrice[]}) => {
-    if (asset === "BTC") {
-        return <div>
-            ${(qty * leverage * ((orderType === "BUY") ? assetPrices.find(e => e.asset === "BTC")?.askPrice! : assetPrices.find(e => e.asset === "BTC")?.bidPrice! )).toFixed(2)}
-        </div>
-    } else {
-        return <div>
-            ${(qty * leverage * ((orderType === "BUY") ? assetPrices.find(e => e.asset === "SOL")?.askPrice! : assetPrices.find(e => e.asset === "SOL")?.bidPrice! )).toFixed(2)}
-        </div>
-    }
+    const ast = assetPrices.find(a => a.asset === asset)
+    if (!ast) return <div>$0.00</div>
+    const price = (orderType === "long") ? ast.askPrice : ast.bidPrice
+    return <div>${(qty*leverage*price).toFixed(2)}</div>
 }
