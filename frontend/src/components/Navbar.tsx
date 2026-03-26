9f1a7e3320734849293f180435553967a6896903
Karmanay Sharma <karmanaysharmaog@gmail.com>
2026-03-26 16:47:57 +0530

    app name add

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div className="flex items-center justify-between w-full p-4 mb-6 ">
       <div className="text-2xl font-semibold bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent tracking-wide">
-        Welcome, User
+        Exness
       </div>
       <UsdBalance />
     </div>
